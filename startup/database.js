eb26d688d3bbb588dfcade1c6c2c2e5b6ec27578
MisterRoy <donald.damoue.etu@esmt.sn>
2022-01-21 16:52:52 +0000

    fixed edge case in connection to database

--- startup/database.js
+++ startup/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const logger = require('../logger/logger');
 
-function connectToDatabase(connectString) {
+async function connectToDatabase(connectString) {
   try {
-    mongoose.connect(connectString);
+    await mongoose.connect(connectString);
     logger.info('Connected to MongoDB');
   } catch (error) {
     logger.error(
